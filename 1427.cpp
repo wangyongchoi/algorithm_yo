6562fbefd4f66c855100750450790d952d2e7c00
Wangyong Choi <choiwangyong@gmail.com>
2017-10-21 00:08:32 +0900

    1427

--- 1427.cpp
+++ 1427.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <functional>
 #include <algorithm>
 using namespace std;
 vector<int> v;
 
 int main()
 {
+	int length = 0;
 	string s;
 	cin >> s;
-	for(int i = 0; i < s.length(); ++i)
+	for(int i = 0; i < s.size(); ++i)
 	{
-		v.push_back(atoi(s[i]));
+		v.push_back(s[i] - '0');
+		length++;
 	}
-	sort(v.begin(), v.end());
-	for(int i = 0 ; v.size(); ++i)
+	sort(v.begin(), v.end(), greater<int>());
+	for(int i = 0 ; i < length; ++i)
 		printf("%d",v[i]);
 	printf("\n");
 	return 0;
