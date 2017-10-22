82b0cd7797cb59719c05fd2c6bdb9cbedc03909d
Wangyong Choi <choiwangyong@gmail.com>
2017-10-22 22:41:01 +0900

    2178

--- 2178.cpp
+++ 2178.cpp
@@ -1,8 +1,13 @@
-#include <stdio.h>
+/*
+전형적인 bfs 문제이지만, scanf와 cin 등 섞어쓰지 말 것!
+또한, 입력 조건을 잘 확인할 것.
+배열 크기를 충분히 잡았는지 확인할 것.
+*/
+#include <iostream>
 #include <queue>
 #include <utility>
 #include <string.h>
-#define MAX 100
+#define MAX 200
 using namespace std;
 int map[MAX][MAX];
 bool check[MAX][MAX];
@@ -14,17 +19,21 @@ int main()
 	memset(check, false, sizeof(check));
 	memset(dist, 0, sizeof(dist));
 	int n,m;
+    cin >> n >> m;
 	for(int i = 0 ; i < n; ++i)
 	{
-		for(int j = 0; j < m; ++j)
+        string s;
+        cin >> s;
+		for(int j = 0; j < s.length(); ++j)
 		{
-			scanf("%d", &map[i][j]);
+            map[i][j] = s[j] - '0';
 		}
 	}
-
+    
 	q.push(make_pair(0,0));
 	check[0][0] = true;
 	dist[0][0] = 1;
+    
 	while(!q.empty())
 	{
 		int curr_x = q.front().first;
@@ -34,7 +43,7 @@ int main()
 		//right
 		int next_x = curr_x;
 		int next_y = curr_y + 1;
-		if(check[next_x][next_y] == false)
+		if(check[next_x][next_y] == false && map[next_x][next_y] == 1)
 		{
             dist[next_x][next_y] = dist[curr_x][curr_y] + 1;
             check[next_x][next_y] = true;
@@ -44,7 +53,7 @@ int main()
         //left
         next_x = curr_x;
         next_y = curr_y - 1;
-        if(check[next_x][next_y] == false)
+        if(check[next_x][next_y] == false && map[next_x][next_y] == 1)
         {
             dist[next_x][next_y] = dist[curr_x][curr_y] + 1;
             check[next_x][next_y] = true;
@@ -54,7 +63,7 @@ int main()
         //up
         next_x = curr_x - 1;
         next_y = curr_y;
-        if(check[next_x][next_y] == false)
+        if(check[next_x][next_y] == false && map[next_x][next_y] == 1)
         {
             dist[next_x][next_y] = dist[curr_x][curr_y] + 1;
             check[next_x][next_y] = true;
@@ -64,7 +73,7 @@ int main()
         //down
         next_x = curr_x + 1;
         next_y = curr_y;
-        if(check[next_x][next_y] == false)
+        if(check[next_x][next_y] == false && map[next_x][next_y] == 1)
         {
             dist[next_x][next_y] = dist[curr_x][curr_y] + 1;
             check[next_x][next_y] = true;
@@ -72,6 +81,6 @@ int main()
         }
 	}
     
-    printf("%d\n",dist[n][m]);
+    cout << dist[n-1][m-1] << endl;
     return 0;
 }
