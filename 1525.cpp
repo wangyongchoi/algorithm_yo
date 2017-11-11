e3596bbb1630da53fe5a19333188c2929fc12dff
Wangyong Choi <choiwangyong@gmail.com>
2017-11-11 13:40:43 +0900

    1525, 9095

---
+++ 1525.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <queue>
+using namespace std;
+int dx[4] = {-1, 0 , 1, 0};
+int dy[4] = {0, -1, 0, 1};
+queue<int> q;
+int main()
+{
+	int start = 0;
+	for(int i = 0; i < 3; ++i)
+	{
+		for(int j = 0 ; j < 3; ++j)
+		{
+			
+		}
+	}
+}
