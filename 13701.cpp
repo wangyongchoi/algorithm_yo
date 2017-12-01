96992975a4925ad20ed8785edd42b72948852a9b
Wangyong Choi <choiwangyong@gmail.com>
2017-12-01 16:25:16 +0900

    13701

--- 13701.cpp
+++ 13701.cpp
@@ -1,17 +1,16 @@
 #include <stdio.h>
-int bits[(1<<20) + 1];
+int b[(1<<20) + 1];
+int n;
+int index, offset;
 int main()
 {
-	int n;
 	while(scanf("%d", &n) == 1)
 	{
-		int x = n / 32;
-		int y = n % 32;
-		if(bits[x] & (1 << y))
+		index = n/32;
+		offset = n%32;
+		if(b[index] & (1 << offset))
 			continue;
 		printf("%d ", n);
-		bits[x] += (1 << y);
+		b[index] += (1 << offset);
 	}
-	printf("\n");
-	return 0;
 }
