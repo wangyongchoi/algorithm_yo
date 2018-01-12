77def4b39862b257705acc01d87f2031f3854fce
Wangyong Choi <choiwangyong@gmail.com>
2018-01-12 11:53:38 +0900

    modify 14503

--- 14503.cpp
+++ 14503.cpp
@@ -1,75 +1,71 @@
 #include <cstdio>
 #define MAX 51
+#define BLANK 0
+#define WALL 1
+#define CLEAR 2
 using namespace std;
-
-int n, m;
-int r, c, d;
+int n,m,r,c,d;
+//n e s w
+int d_row[4] = { -1, 0, 1,  0};
+int d_col[4] = {  0, 1, 0, -1};
 int map[MAX][MAX];
-int dr[4] = {-1, 0, 1,  0}; // n e s w
-int dc[4] = { 0, 1, 0, -1};
 int res = 0;
-
-void go()
-{
-    while(1)
-    {
-        //1
-        if(map[r][c] == 0)
-        {
-            res++;
-            map[r][c] = 2;
-        }
-
-        bool unclear = false;
-        //2
-        for(int i = 0; i < 4; ++i)
-        {
-            int next_d = (d+4 - 1) % 4;
-            int next_r = r + dr[next_d];
-            int next_c = c + dc[next_d];
-
-            //2-1
-            if(map[next_r][next_c] == 0)
-            {
-                d = next_d;
-                r = next_r;
-                c = next_c;
-                unclear = true;
-                break;
-            }//2-2
-            else{
-                d = next_d;
-            }
-        }
-
-        if(!unclear)
-        {
-            //2-4
-            if(map[r-dr[d]][c-dc[d]] == 1)
-            {
-                printf("%d\n", res);
-                break;
-            }//2-3
-            else{
-                r-=dr[d];
-                c-=dc[d];
-            }
-        }
-
-    }
-}
-
+int next_d = 0;
 int main()
 {
     scanf("%d %d", &n, &m);
     scanf("%d %d %d", &r, &c, &d);
-    for(int i = 0; i < n; ++i)
+    for(int i = 0 ; i < n; ++i)
     {
-        for(int j = 0; j < m; ++j)
+        for(int j = 0 ; j < m; ++j)
         {
             scanf("%d", &map[i][j]);
         }
     }
-    go();
-    return 0;
+
+    while(1)
+    {
+        //1
+        if(map[r][c] == BLANK)
+        {
+            map[r][c] = CLEAR;
+            res++;
+        }
+
+        bool all_clear = false;
+        //2
+        for(int i = 0; i < 4; ++i)
+        {
+            next_d = (d+4-1)%4;
+            int next_r = r + d_row[next_d];
+            int next_c = c + d_col[next_d];
+            //2-1
+            if(map[next_r][next_c] == BLANK)
+            {
+                d = next_d;
+                r = next_r;
+                c = next_c;
+                all_clear = true;
+                break;
+            }//2-2
+            else
+            {
+                d = next_d;
+            }
+        }
+        if(!all_clear)
+        {
+            //2-3
+            if(map[r - d_row[d]][c - d_col[d]] == WALL)
+            {
+                printf("%d\n", res);
+                return 0;
+            }//2-4
+            else
+            {
+                r -= d_row[d];
+                c -= d_col[d];
+            }
+        }
+    }
 }
