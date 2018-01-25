b4a7289b4ced8f845ab464043ae7d0dd604152b7
Wangyong Choi <choiwangyong@gmail.com>
2018-01-25 14:24:55 +0900

    10991

---
+++ 10991.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+int n;
+int main()
+{
+    scanf("%d", &n);
+    for(int i = 0 ; i < n; ++i)
+    {
+        for(int j = 0; j < n-i-1; ++j)
+            printf(" ");
+        printf("*");
+        for(int j = 0; j < i; ++j)
+            printf(" *");
+        printf("\n");
+    }
+}
